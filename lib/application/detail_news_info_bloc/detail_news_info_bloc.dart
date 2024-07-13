50a4e5e9f5db1634b1902242d78e081581870550
Nodir Barotov <nodirbek.barotov@opentech.uz>
2024-07-13 16:29:20 +0500

    last updates

--- lib/application/detail_news_info_bloc/detail_news_info_bloc.dart
+++ lib/application/detail_news_info_bloc/detail_news_info_bloc.dart
@@ -1,12 +1,12 @@
-import 'dart:async';
-
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'detail_news_info_event.dart';
+
 part 'detail_news_info_state.dart';
 
-class DetailNewsInfoBloc extends Bloc<DetailNewsInfoEvent, DetailNewsInfoState> {
+class DetailNewsInfoBloc
+    extends Bloc<DetailNewsInfoEvent, DetailNewsInfoState> {
   DetailNewsInfoBloc() : super(DetailNewsInfoInitial()) {
     on<DetailNewsInfoEvent>((event, emit) {
       // TODO: implement event handler
